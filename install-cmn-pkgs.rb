ba5b7c8b264953d59b1d6903651cf0094993c885
kw.ryu <kw.ryu@anypointmedia.com>
2020-12-13 17:41:23 +0900

    Add install-cmn-pkgs.rb

---
+++ install-cmn-pkgs.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+
+def run cmd
+  puts "=============== execute: #{cmd}"
+  system(cmd)
+  exitNo = $?.exitstatus
+  exit(exitNo) if exitNo != 0
+end
+
+pkgs = [
+  'magic-enum',
+  'nameof',
+  'openssl',
+  'poco',
+  'pugixml',
+  'sqlite3',
+  'toml11',
+]
+
+pkg.each do |pkg|
+  cmd = "./vcpkg install #{pkg}'"
+end
